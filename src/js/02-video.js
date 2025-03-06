f5b513a804267b8978f933cbb81352e2344c5c54
nata20019 <nbernikova68@gmail.com>
2025-03-06 18:11:22 +0100

    mod8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,45 @@
+// import Player from '@vimeo/player';
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+// console.log(Player);
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
 
+// const onPlay = function (data) {};
+
+// player.on('play', onPlay);
+// const currentTime = {
+//   duration: 61.857,
+//   percent: 0.049,
+//   seconds: 3.034,
+// };
+
+// iframe.addEventListener('timeupdate', function () {
+//   const currentTime = iframe.currentTime;
+//   const totalTime = iframe.duration;
+//   console.log(currentTime);
+
+//   // Отримати елемент для відображення часу відтворення
+//   const timeDisplay = document.querySelector('#time-display');
+
+//   // Змінити текст елементу з часом відтворення
+//   timeDisplay.textContent = `${currentTime} / ${totalTime}`;
+// });
+
+// player
+//   .setCurrentTime(30.456)
+//   .then(function (seconds) {
+//     // seconds = the actual time that the player seeked to
+//   })
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         // the time was less than 0 or greater than the video’s duration
+//         break;
+
+//       default:
+//         // some other error occurred
+//         break;
+//     }
+//   });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+
+const localStorageKey = 'feedback-form-state';
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onTextInput, 500));
+populateTextarea();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  form.reset();
+  localStorage.removeItem(localStorageKey);
+}
+
+function onTextInput(evt) {
+  evt.preventDefault();
+  const email = input.value;
+  const message = textarea.value;
+  localStorage.setItem(localStorageKey, JSON.stringify({ email, message }));
+
+  const formData = { email, message };
+  console.log(formData);
+}
+function populateTextarea() {
+  const saveMessage = localStorage.getItem(localStorageKey);
+  if (saveMessage) {
+    const parsedMessage = JSON.parse(saveMessage);
+    input.value = parsedMessage.email;
+    textarea.value = parsedMessage.message;
+  }
+}
