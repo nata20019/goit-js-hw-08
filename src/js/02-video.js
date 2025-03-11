09ec1d27eea8967697499c34bc5213bcbb190d80
nata20019 <nbernikova68@gmail.com>
2025-03-11 19:50:06 +0100

    mod8 newww

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,45 +1,20 @@
-// import Player from '@vimeo/player';
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-// console.log(Player);
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-// const onPlay = function (data) {};
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const localStorageKey = 'videoplayer-current-time';
 
-// player.on('play', onPlay);
-// const currentTime = {
-//   duration: 61.857,
-//   percent: 0.049,
-//   seconds: 3.034,
-// };
+player.on('play', function () {
+  console.log('played the video!');
+});
+player.on('timeupdate', throttle(timeIs, 500));
 
-// iframe.addEventListener('timeupdate', function () {
-//   const currentTime = iframe.currentTime;
-//   const totalTime = iframe.duration;
-//   console.log(currentTime);
+function timeIs(evt) {
+  console.log(evt);
+  console.log(evt.seconds);
 
-//   // Отримати елемент для відображення часу відтворення
-//   const timeDisplay = document.querySelector('#time-display');
-
-//   // Змінити текст елементу з часом відтворення
-//   timeDisplay.textContent = `${currentTime} / ${totalTime}`;
-// });
-
-// player
-//   .setCurrentTime(30.456)
-//   .then(function (seconds) {
-//     // seconds = the actual time that the player seeked to
-//   })
-//   .catch(function (error) {
-//     switch (error.name) {
-//       case 'RangeError':
-//         // the time was less than 0 or greater than the video’s duration
-//         break;
-
-//       default:
-//         // some other error occurred
-//         break;
-//     }
-//   });
+  const currentTime = evt.seconds;
+  localStorage.setItem(localStorageKey, JSON.stringify(currentTime));
+}
+console.log(timeIs(evt));
